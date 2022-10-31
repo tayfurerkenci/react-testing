8cd66c22b391e947ac5abdd5aa864f24536cc35f
Tayfur Erkenci <terkenci@gmail.com>
2022-10-31 21:49:42 +0300

    implemented react testing lib getbyrole

---
+++ src/components/application/Application.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from "@testing-library/react";
+import { Application } from "./Application";
+
+describe('Application', () => {
+  test('renders correctly', () => {
+    render(<Application />);
+
+    const pageHeading = screen.getByRole('heading', {
+      // by the text content
+      // name: 'Job Application Form'
+      
+      // by the heading level
+      level: 1
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole('heading', {
+      // by the text content
+      // name: 'Section 1'
+
+      // by the heading level
+      level: 2
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    const nameElement = screen.getByRole('textbox', {
+      name: 'Name',
+    });
+    expect(nameElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole('textbox', {
+      name: 'Bio',
+    });
+    expect(bioElement).toBeInTheDocument();
+
+    const jobLocationElement = screen.getByRole('combobox');
+    expect(jobLocationElement).toBeInTheDocument();
+
+    const termsElement = screen.getByRole('checkbox');
+    expect(termsElement).toBeInTheDocument();
+
+    const submitButtonElement = screen.getByRole('button');
+    expect(submitButtonElement).toBeInTheDocument(); 
+  });
+})
---
+++ src/components/application/Application.tsx
@@ -0,0 +1,35 @@
+export const Application = () => {
+  return (
+    <>
+      <h1>Job Application Form</h1>
+      <h2>Section 1</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" />
+        </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <textarea id="bio" name="bio" />
+        </div>
+        <div>
+          <label htmlFor="job-location">Job Location</label>
+          <select id="job-location">
+            <option value="">Select a country</option>
+            <option value="US">United States</option>
+            <option value="GB">United Kingdom</option>
+            <option value="CA">Canada</option>
+            <option value="UN">India</option>
+            <option value="AU">Australia</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" id="terms" /> I aggree to the terms and conditions
+          </label>
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
+  );
+};
