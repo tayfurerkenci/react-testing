215a9fb273f58afbd375746ff643d513e13e939d
Tayfur Erkenci <terkenci@gmail.com>
2022-11-01 20:38:53 +0300

    tested pointer interactions

---
+++ src/components/counter/Counter.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { Counter } from './Counter';
+
+describe('Counter', () => {
+  test('renders correctly', () => {
+    render(<Counter />);
+    
+    const countElement = screen.getByRole('heading');
+    expect(countElement).toBeInTheDocument();
+    const incrementButton = screen.getByRole('button', {
+      name: 'Increment',
+    });
+    expect(incrementButton).toBeInTheDocument();
+  });
+
+  test('renders a count of 0', () => {
+    render(<Counter />);
+
+    const countElement = screen.getByRole('heading');
+    expect(countElement).toHaveTextContent('0');
+  });
+
+  test('renders a count of 1 after clicking the icrement button', async () => {
+    user.setup();
+    render(<Counter />);
+
+    const incrementButton = screen.getByRole('button', {
+      name: 'Increment',
+    });
+    await user.click(incrementButton);
+    const countElement = screen.getByRole('heading');
+    expect(countElement).toHaveTextContent('1');
+  });
+
+})
