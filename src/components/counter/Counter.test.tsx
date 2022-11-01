5dc5b7e32439973c8b88a2b533bfb43096080ccc
Tayfur Erkenci <terkenci@gmail.com>
2022-11-01 21:20:05 +0300

    tested keyboard interactions

--- src/components/counter/Counter.test.tsx
+++ src/components/counter/Counter.test.tsx
@@ -33,4 +33,41 @@ describe('Counter', () => {
     expect(countElement).toHaveTextContent('1');
   });
 
+  test('renders a count of 10 after clicking the set button', async () => {
+    user.setup();
+    render(<Counter />);
+
+    const amountInput = screen.getByRole('spinbutton');
+    await user.type(amountInput, '10');
+    expect(amountInput).toHaveValue(10);
+    const setButton = screen.getByRole('button', {
+      name: 'Set'
+    });
+    await user.click(setButton);
+    const countElement = screen.getByRole('heading');
+    expect(countElement).toHaveTextContent('10');
+  });
+
+  test('elements are focused in the right order', async () => {
+    user.setup();
+    render(<Counter />);
+
+    const amountInput = screen.getByRole('spinbutton');
+    const setButton = screen.getByRole('button', {
+      name: 'Set'
+    });
+    const incrementButton = screen.getByRole('button', {
+      name: 'Increment',
+    });
+
+    await user.tab();
+    expect(incrementButton).toHaveFocus();
+
+    await user.tab();
+    expect(amountInput).toHaveFocus();
+
+    await user.tab();
+    expect(setButton).toHaveFocus();
+  });
+
 })
